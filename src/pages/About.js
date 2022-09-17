1bfb891fd8b0d67caf8f2e31d167d99cd17cd22c
Rahul Mallick <rahulmallick1115@gmail.com>
2022-09-17 13:53:58 +0530

    second commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste obcaecati
+        laudantium illo inventore ducimus sit sapiente, deleniti laboriosam
+        tempore voluptate. Eaque sunt harum nobis ut saepe, commodi, veritatis
+        accusantium vero ipsam unde maxime incidunt impedit corrupti amet.
+        Laborum, tempora blanditiis!
+      </p>
+    </section>
+  );
+};
+
+export default About;
